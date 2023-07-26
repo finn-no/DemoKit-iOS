44e98a81c751991aaab44ac0b51b451750bb1b9a
Bastian Stien <bastian.stien@gmail.com>
2023-07-26 16:54:16 +0200

    Place view containers as subtypes of ViewControllerMapper

---
+++ Sources/DemoKit/ViewContainer/ViewControllerMapper+UIViewContainer.swift
@@ -0,0 +1,49 @@
+import Foundation
+import UIKit
+
+extension ViewControllerMapper {
+    class UIViewContainer: UIViewController {
+
+        // MARK: - Private properties
+
+        private let demoable: any Demoable
+
+        // MARK: - Init
+
+        init(demoable: any Demoable) {
+            self.demoable = demoable
+            super.init(nibName: nil, bundle: nil)
+        }
+
+        required init?(coder aDecoder: NSCoder) {
+            fatalError()
+        }
+
+        // MARK: - Lifecycle
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setup()
+        }
+
+        // MARK: - Setup
+
+        private func setup() {
+            view.backgroundColor = .systemBackground
+
+            // Insert demo view.
+            guard let demoView = demoable as? UIView else {
+                fatalError("⛔️ \(String(describing: Self.self)).\(#function): Given Demoable is not of type `UIView`.")
+            }
+            demoView.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(demoView)
+
+            NSLayoutConstraint.activate([
+                demoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                demoView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                demoView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                demoView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            ])
+        }
+    }
+}
--- Sources/DemoKit/ViewContainer/ViewControllerMapper.swift
+++ Sources/DemoKit/ViewContainer/ViewControllerMapper.swift
@@ -3,17 +3,19 @@ import SwiftUI
 import UIKit
 
 struct ViewControllerMapper {
+    private init() {}
+
     static func viewController(for demoable: any Demoable) -> UIViewController {
         switch demoable {
         case _ as UIView:
-            return UIViewDemoViewController(demoable: demoable)
+            return UIViewContainer(demoable: demoable)
         case let demoView as any View:
             let anyView = AnyView(demoView)
-            return SwiftUIDemoViewController(rootView: anyView, demoable: demoable)
+            return SwiftUIContainer(rootView: anyView, demoable: demoable)
         case let previewProvider as any PreviewProvider:
             let previews = type(of: previewProvider).previews
             let anyView = AnyView(previews)
-            return SwiftUIDemoViewController(rootView: anyView, demoable: demoable)
+            return SwiftUIContainer(rootView: anyView, demoable: demoable)
         case let demoViewController as UIViewController:
             return demoViewController
         default:
