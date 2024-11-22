606a66a77c150d76d7f5985b1c6179c3d9ecb19d
Bastian Stien <bastian.stien@gmail.com>
2024-11-22 12:44:35 +0100

    TweakingOption: Docs

--- Sources/DemoKit/Public/Protocols/TweakingOption.swift
+++ Sources/DemoKit/Public/Protocols/TweakingOption.swift
@@ -3,12 +3,17 @@ import Foundation
 /// Represents a specific tweak/configuration/setup for a `TweakableDemo`.
 @MainActor
 public protocol TweakingOption {
+    /// The identifier for this tweak.
+    /// The last tweak selection will be saved to `UserDefaults` so a re-run of the demo app will present the last viewed demo, with the last selected tweak.
     var identifier: String { get }
+
+    /// A title for this tweak that'll be presented within the list of tweaks for a `TweakableDemo`.
     var title: String { get }
 }
 
 // MARK: - Default values
 
+/// Default values when `TweakingOption` implements `RawRepresentable<String>`, i.e. an enum.
 public extension TweakingOption where Self: RawRepresentable, RawValue == String {
     var identifier: String {
         rawValue
