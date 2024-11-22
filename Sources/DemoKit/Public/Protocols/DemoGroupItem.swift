435af8afe12b3aa0cdb83a0ac6205c848b0322cc
Bastian Stien <bastian.stien@gmail.com>
2024-11-22 12:23:23 +0100

    DemoGroupItem: Remove unused property groupItemIdentifier

--- Sources/DemoKit/Public/Protocols/DemoGroupItem.swift
+++ Sources/DemoKit/Public/Protocols/DemoGroupItem.swift
@@ -3,17 +3,12 @@ import Foundation
 /// Represents a demo for a single item within a `DemoGroup`.
 @MainActor
 public protocol DemoGroupItem {
-    var groupItemIdentifier: String { get }
     var groupItemTitle: String { get }
 }
 
 // MARK: - Default values
 
 public extension DemoGroupItem where Self: RawRepresentable, RawValue == String {
-    var groupItemIdentifier: String {
-        rawValue
-    }
-
     var groupItemTitle: String {
         rawValue.capitalizingFirstLetter
     }
