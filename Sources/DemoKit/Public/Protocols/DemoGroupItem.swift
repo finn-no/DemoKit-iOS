bf50f7e507a24a8f40951169b5c8d48ecc6d827f
Bastian Stien <bastian.stien@gmail.com>
2024-11-22 12:25:40 +0100

    DemoGroupItem: Docs

--- Sources/DemoKit/Public/Protocols/DemoGroupItem.swift
+++ Sources/DemoKit/Public/Protocols/DemoGroupItem.swift
@@ -1,13 +1,15 @@
 import Foundation
 
-/// Represents a demo for a single item within a `DemoGroup`.
+/// Provides info for list presentation for a demo within a `DemoGroup`.
 @MainActor
 public protocol DemoGroupItem {
+    /// Title to present for this demo when listing a `DemoGroup`.
     var groupItemTitle: String { get }
 }
 
 // MARK: - Default values
 
+/// Default value when `DemoGroupItem` implements `RawRepresentable<String>`, i.e. an enum.
 public extension DemoGroupItem where Self: RawRepresentable, RawValue == String {
     var groupItemTitle: String {
         rawValue.capitalizingFirstLetter
